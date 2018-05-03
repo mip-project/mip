1b84568e29d5fcebe4422423d64b0ac692075101
zoumiaojiang <zoumiaojiang@gmail.com>
2018-05-03 10:47:08 +0800

    update simple demo

--- examples/simple/tree.js
+++ examples/simple/tree.js
@@ -5,92 +5,48 @@
 
 /* global MIP */
 
-var data = {
-    name: 'My Tree',
-    children: [
-        {
-            name: 'hello'
-        },
-        {
-            name: 'wat'
-        },
-        {
-            name: 'child folder',
-            children: [
-                {
-                    name: 'child folder',
-                    children: [
-                        {
-                            name: 'hello'
-                        },
-                        {
-                            name: 'wat'
-                        }
-                    ]
-                },
-                {
-                    name: 'hello'
-                },
-                {
-                    name: 'wat'
-                },
-                {
-                    name: 'child folder',
-                    children: [
-                        {
-                            name: 'hello'
-                        },
-                        {
-                            name: 'wat'
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-};
-
 // define the item component
-MIP.component('item', {
+MIP.customElement('mip-item', {
     template: '#item-template',
     props: {
-        model: Object
+        model: {
+            default() {
+                return {};
+            },
+            type: Object
+        }
     },
-    data: function () {
+    data() {
         return {
             open: false
         };
     },
     computed: {
-        isFolder: function () {
+        isFolder() {
             return this.model.children && this.model.children.length;
         }
     },
     methods: {
-        toggle: function () {
+        toggle() {
             if (this.isFolder) {
                 this.open = !this.open;
             }
         },
-        changeType: function () {
+        changeType() {
             if (!this.isFolder) {
                 MIP.set(this.model, 'children', []);
                 this.addChild();
                 this.open = true;
             }
         },
-        addChild: function () {
+        addChild() {
             this.model.children.push({
                 name: 'new stuff'
             });
-        }
-    }
-});
+        },
 
-// boot up the demo
-new MIP({
-    el: '#demo',
-    data: {
-        treeData: data
+        stringify(data) {
+            return JSON.stringify(data);
+        }
     }
 });
