7049256bbb3a9808160be1b17a4e702a5ca130b2
zhangzhiqiang04 <zhangzhiqiang04@baidu.com>
2018-05-08 14:03:34 +0800

    sandbox调整

--- examples/sandbox/index.js
+++ examples/sandbox/index.js
@@ -1,41 +1,42 @@
-import sandbox from '../../src/core/core/util/sandbox';
+(function () {
+    let {window, document} = mip.sandbox;
+    let {
+        alert,
+        close,
+        confirm,
+        prompt,
+        setTimeout,
+        setInterval,
+        eval
+    } = window;
 
-let {window, document} = sandbox;
-let {
-    alert,
-    close,
-    confirm,
-    prompt,
-    setTimeout,
-    setInterval
-    // eval // eval打包不给通过
-} = window;
+    console.log(
+        'window: ', window, '\n',
+        'document: ', document, '\n',
+        'window.document: ', window.document, '\n',
+        'alert: ', alert, '\n',
+        'close: ', close, '\n',
+        'confirm: ', confirm, '\n',
+        'prompt: ', prompt, '\n',
+        'eval: ', eval, '\n',
+        'document.createElement: ', document.createElement, '\n',
+        'document.createElementNS: ', document.createElementNS, '\n',
+        'document.write: ', document.write, '\n',
+        'document.writeln: ', document.writeln
+    );
 
-console.log(
-    'window: ', window, '\n',
-    'document: ', document, '\n',
-    'window.document: ', window.document, '\n',
-    'alert: ', alert, '\n',
-    'close: ', close, '\n',
-    'confirm: ', confirm, '\n',
-    'prompt: ', prompt, '\n',
-    'eval: ', window.eval, '\n',
-    'document.createElement: ', document.createElement, '\n',
-    'document.createElementNS: ', document.createElementNS, '\n',
-    'document.write: ', document.write, '\n',
-    'document.writeln: ', document.writeln
-);
+    setTimeout(function () {
+        console.log('我是setTimeout中使用的this：', this);
+    });
 
-setTimeout(function () {
-    console.log('我是setTimeout中使用的this：', this);
-});
+    let interval = setInterval(function () {
+        console.log('我是setInterval中使用的this：', this);
+    }, 1000);
 
-let interval = setInterval(function () {
-    console.log('我是setInterval中使用的this：', this);
-}, 1000);
+    setTimeout(function () {
+        console.log('清除interval任务');
+        clearInterval(interval);
+    }, 2000);
 
-setTimeout(function () {
-    console.log('清除interval任务');
-    clearInterval(interval);
-}, 2000);
+})();
 
