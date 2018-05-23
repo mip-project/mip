4b8689145a2a036ae6465c245fa3338eaed7543e
zhangzhiqiang37 <zhiqiangzhang37@163.com>
2018-05-23 16:07:14 +0800

    使用customElement的方式来写内置组件

---
+++ src/custom-element-components/mip-iframe.js
@@ -0,0 +1,41 @@
+/**
+ * @file mip-iframe
+ * @author zhangzhiqiang(zhiqiangzhang37@163.com)
+ */
+
+import util from '../util';
+import customElementClass from '../mip1-polyfill/customElement';
+let customElem = customElementClass.create();
+
+let attrList = ['allowfullscreen', 'allowtransparency', 'sandbox'];
+customElem.prototype.build = function () {
+    let element = this.element;
+    let src = element.getAttribute('src');
+    let srcdoc = element.getAttribute('srcdoc');
+    if (srcdoc) {
+        src = 'data:text/html;charset=utf-8;base64,' + window.btoa(srcdoc);
+    }
+
+    let height = element.getAttribute('height');
+    let width = element.getAttribute('width') || '100%';
+
+    if (!src || !height) {
+        return;
+    }
+
+    let iframe = document.createElement('iframe');
+    iframe.frameBorder = '0';
+    iframe.scrolling = 'no';
+    util.css(iframe, {
+        width,
+        height
+    });
+
+    this.applyFillContent(iframe);
+    iframe.src = src;
+
+    this.expendAttr(attrList, iframe);
+    element.appendChild(iframe);
+};
+
+export default customElem;
